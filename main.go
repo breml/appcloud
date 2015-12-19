46b11e44e7db6d0604451338b58b67b757d02abc
Lucas Bremgartner <lucas@bremis.ch>
2015-12-19 23:07:55 +0100

    Working go version of mongodb example

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"regexp"
 
 	"github.com/breml/appcloud/Godeps/_workspace/src/gopkg.in/mgo.v2"
 	"github.com/breml/appcloud/Godeps/_workspace/src/gopkg.in/mgo.v2/bson"
@@ -22,9 +23,10 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer session.Close()
 
+	c := session.DB("weatherDB").C("weatherCOLL")
+
 	var result Temperature
 
-	c := session.DB("weatherDB").C("weatherCOLL")
 	err = c.Find(nil).One(&result)
 	if err != nil {
 		fmt.Printf("MongoDB find err %v\n", err)
@@ -34,30 +36,36 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "<h1>How is the weather ? </h1>")
 	fmt.Fprintf(w, "Hello world! <br><hr> The weather is "+result.Temperature+" today")
-	// res.write('<br><hr><form action=\'/temp\'><h3>Change weather</h3><br><input type=text name=temp value=hot><br><input type=submit value=\'Change the weather...\'></form>');
+	fmt.Fprintf(w, "<br><hr><form action='/temp'><h3>Change weather</h3><br><input type=text name=temp value=hot><br><input type=submit value='Change the weather...'></form>")
 }
 
 func tempHandler(w http.ResponseWriter, r *http.Request) {
-	/*
-	   	app.get('/temp', function (req, res) { // telling nodeJs to get all commands from /temp into this function
-
-	   	mongoClient.connect( "mongodb://localhost:27017/weatherDB" , function(err, db) { // connect to the local database
-	   	  	if(err) { return console.dir(err); } // check if connection is ok, else output
-
-	   	  	db.collection('weatherCOLL').drop(); // drop the collection if existing
-	     		db.collection('weatherCOLL').insert( {"temperature": req.query.temp } ) // add a new object with "temperature"
-	   		res.send("done <hr> <a href=\'/\'>back</a>");
-	   		res.end();
-	     		db.close(); // close the Database connection
-	     	});
-	   });
-	*/
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	queryValues := r.URL.Query()
-	// TODO: check if weather is only chars: [a-zA-Z0-9]
+
 	if len(queryValues["temp"]) > 0 && queryValues["temp"][0] != "" {
-		fmt.Fprintf(w, "The weather is now: %s\n", queryValues["temp"][0])
+		re := regexp.MustCompile("[[:^alnum:]]")
+		temperature := re.ReplaceAllString(queryValues["temp"][0], "")
+
+		session, err := mgo.Dial("mongodb://localhost:27017/weatherDB")
+		if err != nil {
+			fmt.Printf("MongoDB dial err %v\n", err)
+			return
+		}
+		defer session.Close()
+
+		c := session.DB("weatherDB").C("weatherCOLL")
+
+		err = c.DropCollection()
+		if err != nil {
+			fmt.Printf("MongoDB drop collection err %v\n", err)
+			return
+		}
+
+		c.Insert(Temperature{Temperature: temperature})
+
+		fmt.Fprintf(w, "The weather is now: %s\n", temperature)
 		fmt.Fprintf(w, "done <hr> <a href='/'>back</a>")
 	} else {
 		fmt.Fprintf(w, "error: no new temp set <hr> <a href='/'>back</a>")
