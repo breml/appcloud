2476c230794ee0013503b929d5cde88ab36ba34f
Lucas Bremgartner <lucas@bremis.ch>
2015-12-21 21:31:31 +0100

    Some cleanup

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"regexp"
@@ -13,21 +14,28 @@ import (
 	"github.com/cloudfoundry-community/go-cfenv"
 )
 
+const prefixDelim = ": "
+
 type Temperature struct {
 	ID          bson.ObjectId `bson:"_id,omitempty"`
 	Temperature string        `bson:"temperature"`
 }
 
+// Initialize global service variables with default values for local
+// deployment. If executed in App Cloud, these variables will be replaced
+// with the values from ENV.
 var (
+	name = "appcloud"
+	port = "3000"
+
 	mongouri = "mongodb://localhost:27017/weatherDB"
 	mongodb  = "weatherDB"
-	port     = "3000"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	session, err := mgo.Dial(mongouri)
 	if err != nil {
-		fmt.Printf("MongoDB dial err %v\n", err)
+		log.Printf("MongoDB dial err %v\n", err)
 		return
 	}
 	defer session.Close()
@@ -39,20 +47,21 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 	err = c.Find(nil).One(&result)
 	if err != nil {
-		fmt.Printf("MongoDB find err %v\n", err)
+		log.Printf("MongoDB find err %v\n", err)
 	} else {
 		curTemp = result.Temperature
 	}
+
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
+
 	fmt.Fprintf(w, "<h1>How is the weather ? </h1>")
-	fmt.Fprintf(w, "Hello world! <br><hr> The weather is "+curTemp+" today")
+	fmt.Fprintf(w, "Hello world! <br><hr> The weather is %s today.", curTemp)
 	fmt.Fprintf(w, "<br><hr><form action='/temp'><h3>Change weather</h3><br><input type=text name=temp value=hot><br><input type=submit value='Change the weather...'></form>")
 }
 
 func tempHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
+
 	queryValues := r.URL.Query()
 
 	if len(queryValues["temp"]) > 0 && queryValues["temp"][0] != "" {
@@ -61,7 +70,7 @@ func tempHandler(w http.ResponseWriter, r *http.Request) {
 
 		session, err := mgo.Dial(mongouri)
 		if err != nil {
-			fmt.Printf("MongoDB dial err %v\n", err)
+			log.Printf("MongoDB dial err %v\n", err)
 			return
 		}
 		defer session.Close()
@@ -70,52 +79,57 @@ func tempHandler(w http.ResponseWriter, r *http.Request) {
 
 		err = c.DropCollection()
 		if err != nil {
-			fmt.Printf("MongoDB drop collection err %v\n", err)
+			log.Printf("MongoDB drop collection err %v\n", err)
 		}
 
 		err = c.Insert(Temperature{Temperature: temperature})
 		if err != nil {
-			fmt.Printf("MongoDB insert err %v\n", err)
+			log.Printf("MongoDB insert err %v\n", err)
 		} else {
 			fmt.Fprintf(w, "The weather is now: %s\n", temperature)
 		}
-		fmt.Fprintf(w, "done <hr> <a href='/'>back</a>")
 	} else {
 		fmt.Fprintf(w, "error: no new temp set <hr> <a href='/'>back</a>")
 	}
+	fmt.Fprintf(w, "<hr> <a href='/'>back</a>")
 }
 
 func main() {
-	// Get MongoDB settings from ENV
+	// Setup logging
+	log.SetFlags(0)
+	log.SetOutput(os.Stdout)
+	log.SetPrefix(name + prefixDelim)
+
+	// Get settings from ENV if present (e. g. if in App Cloud)
 	appEnv, err := cfenv.Current()
 	if err == nil {
-		// In App Cloud
+		// Port to bind web app
 		port = strconv.Itoa(appEnv.Port)
 
-		// Service
+		name = appEnv.Name
+		log.SetPrefix(name + prefixDelim)
+
+		// MongoDB Service
 		mgoService, err := appEnv.Services.WithName("mongodb")
 		if err == nil {
 			var ok bool
 			mongouri, ok = mgoService.Credentials["uri"].(string)
 			if !ok {
-				fmt.Printf("No valid MongoDB uri\n")
-				os.Exit(1)
+				log.Fatalf("No valid MongoDB uri\n")
 			}
 			mongodb, ok = mgoService.Credentials["database"].(string)
 			if !ok {
-				fmt.Printf("No valid MongoDB database\n")
-				os.Exit(1)
+				log.Fatalf("No valid MongoDB database\n")
 			}
 		}
 	}
 
-	fmt.Printf("Use port: %s\n", port)
+	log.Printf("Use port: %s\n", port)
 
 	http.HandleFunc("/", helloHandler)
 	http.HandleFunc("/temp", tempHandler)
 	err = http.ListenAndServe(":"+port, nil)
 	if err != nil {
-		fmt.Printf("ListenAndServe error: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("ListenAndServe error: %v\n", err)
 	}
 }
